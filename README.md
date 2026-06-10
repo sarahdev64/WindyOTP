80dc951f289b3b023b986e590f3f879096dad793
robbryandev <robbryandev@gmail.com>
2026-06-10 12:33:28 -0700

    Fix otp-card overflow

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Then serve the new dist folder at ``apps/site/dist``
 - Expo
 - Typescript
 - NativeWind
-- [VanillaTOTP (My Library)](https://github.com/robbryandev/vanilla-totp)
+- [VanillaTOTP (My Library)](https://github.com/sarahdev64/vanilla-totp)
 - protobufjs
 - react-native-qrcode-svg
 
@@ -58,18 +58,3 @@ Then serve the new dist folder at ``apps/site/dist``
 - Javascript
 - Typescript
 - Vue
-
-## Known Bugs
-* None
-
-## License
-
-MIT License
-
-Copyright (c) 2023 Robert Bryan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, ScrollView } from 'react-native';
-import { Link } from 'expo-router';
-import { type CodeList, getCodes } from '../utils/codes';
-import { useEffect, useState } from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {Text, View, FlatList} from 'react-native';
+import {Link} from 'expo-router';
+import {type CodeList, getCodes} from '../utils/codes';
+import {useEffect, useState} from 'react';
 import OtpCard from '../components/OtpCard';
 
 export default function HomePage() {
@@ -18,7 +18,7 @@ export default function HomePage() {
   }, [refresh])
 
   return (
-    <View className="min-h-screen pb-80">
+    <View className="bg-backdrop flex flex-col h-full">
       <View className="bg-nav p-4 pt-6 flex flex-row justify-evenly">
         <Text className="font-semibold text-2xl text-txt px-4 py-2">WindyOTP</Text>
         <View className="rounded-full bg-backdrop">
@@ -28,21 +28,23 @@ export default function HomePage() {
           <Link href="/exports" className="font-semibold text-3xl text-txt px-4 py-2">{"\u2197"}</Link>
         </View>
       </View>
-      <View className="bg-backdrop min-h-full">
-        <ScrollView className='flex flex-grow'>
-          {codes && codes.codes ? Object.keys(codes.codes).map((key: string, index: number) => {
-            return (
-              <OtpCard
-                key={index}
-                name={codes.codes[key].account}
-                data={codes.codes[key]}
-                setRefresh={setRefresh}
-              />
-            )
-          }) : null}
-        </ScrollView>
-      </View>
-      <StatusBar style="auto" />
+      <FlatList
+        scrollEnabled={true}
+        scrollToOverflowEnabled={true}
+        data={codes && codes.codes ? Object.keys(codes.codes) : null}
+        renderItem={({item, index}) => {
+          return (
+            <OtpCard
+              key={index}
+              name={codes.codes[item].account}
+              data={codes.codes[item]}
+              setRefresh={setRefresh}
+            />
+          )
+        }}
+        keyExtractor={item => item}
+      />
+      <StatusBar style="auto"/>
     </View>
   );
 }
