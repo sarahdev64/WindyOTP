2b1fb521c0230cb9e8d025db2ee2ff8defc4eb98
robbryandev <robbryandev@gmail.com>
2026-06-12 10:27:23 -0700

    update routing

--- apps/site/src/main.ts
+++ apps/site/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "./assets/global.css"
 
-import "./style.css"
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+app.use(router)
+
+app.mount('#app')
--- apps/site/src/router/index.ts
+++ apps/site/src/router/index.ts
@@ -1,17 +1,14 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "@/router/index.vue";
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: HomeView
+    }
+  ],
 })
 
 export default router
